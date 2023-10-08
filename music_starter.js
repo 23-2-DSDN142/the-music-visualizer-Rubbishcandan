f43d9411f5926ddabe024f1d1811aab44d69b4e5
Rubbishcandan <132533266+Rubbishcandan@users.noreply.github.com>
2023-10-09 10:18:32 +1300

    Update music_starter.js

--- music_starter.js
+++ music_starter.js
@@ -5,13 +5,17 @@ function draw_one_frame(words, vocal, drum, bass, other, counter) {
   textFont('Helvetica'); // please use CSS safe fonts
   rectMode(CENTER)
   textSize(24);
+
+ var BuildingHeight = [30,50,70]; 
+ var BuildingColour = ['#dd78ae','#20b0b1','#f7f383','#ad7cb8']
+ let BuildingY = 400
  push()
  stroke('#E93CAC');
   let lengthofline3 = 1300
   let linestart3 = 0
   let lineend3 = linestart3 + lengthofline3
   let linestep3 = 20;
-  let linestep4 = 15;
+  let linestep4 = 20;
 
   for(let i=1; i < 90; i++){
     line(linestart3, linestep3 * i, lineend3, linestep3 * i)
@@ -71,15 +75,49 @@ pop()
 
 
    push()
-   fill(159, 83, 232,0)
-   rect(635,410,1300,10)
+   noStroke()
+   fill(159, 83, 232,255)
+   rect(635,410,1300,5)
+   pop()
+   
+   push()
+   noStroke()
+   fill(159, 83, 232,255)
+   rect(640,395,200,5)
+   pop()
+
+   push()
+   noStroke()
+   fill(159, 83, 232,255)
+   rect(640,380,190,5)
    pop()
 
    push()
    fill('#4b1445')
    quad(0,720,1280,720,660,400,620,400)
    pop()
-  
+
+   push()
+   strokeWeight(2)
+   fill(BuildingColour[1])
+   rect(50,BuildingY+5,40,BuildingHeight[0])
+   fill(BuildingColour[0])
+   rect(100,BuildingY-15,50,BuildingHeight[2])
+   fill(BuildingColour[1])
+   rect(160,BuildingY-5,60,BuildingHeight[1])
+   fill(BuildingColour[2])
+   rect(215,BuildingY+5,40,BuildingHeight[0])
+   fill(BuildingColour[3])
+   rect(270,BuildingY-5,60,BuildingHeight[1])
+   fill(BuildingColour[2])
+   rect(330,BuildingY-15,50,BuildingHeight[2])
+   fill(BuildingColour[3])
+   rect(380,BuildingY+5,40,BuildingHeight[0])
+   fill(BuildingColour[0])
+   rect(430 ,BuildingY-15,50,BuildingHeight[2])
+   fill(BuildingColour[2])
+   rect(490,BuildingY-5,60,BuildingHeight[1])
+   pop()
    //fill(0)
    //rect(width/3, barheight,50,100)
   // rect(width/1.5, barheight,50,100)
@@ -107,7 +145,7 @@ pop()
    //fill(200, 200, 200);
    //rect(bar_pos_x, height / 2 + 4 * bar_spacing, 4 * other, bar_height);
    //fill(0);
-   //text("other", bar_pos_x, height / 2 + 4 * bar_spacing + 8);
+   //text"other", bar_pos_x, height / 2 + 4 * bar_spacing + 8);
    //fill(255, 255, 0);
  
    // display "words"
